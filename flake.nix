5454d97d778bdcadba4fd2fadabbbb1f0e63fad8
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-03-19 10:51:50 -0700

    add treefmt

--- flake.nix
+++ flake.nix
@@ -1,26 +1,34 @@
 {
   description = "My personal nix repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      systems,
+      treefmt-nix,
+    }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        # "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
+      packages = eachSystem (
+        pkgs:
         pkgs.lib.mapAttrs (name: _: pkgs.callPackage ./pkgs/${name}/package.nix { }) (
           builtins.readDir ./pkgs
         )
       );
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      formatter = eachSystem (
+        pkgs:
+        treefmt-nix.lib.mkWrapper pkgs {
+          programs.nixfmt.enable = true;
+        }
+      );
     };
 }
