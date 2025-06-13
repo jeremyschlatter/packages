e1c22483620625a8400683eecad94781c5553ae2
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-06-13 11:33:27 -0700

    add patched direnv

---
+++ pkgs/direnv/package.nix
@@ -0,0 +1,12 @@
+{
+  direnv,
+  fetchpatch,
+}:
+
+direnv.overrideAttrs (oldAttrs: {
+  patches = (oldAttrs.patches or []) ++ [(fetchpatch {
+    name = "error-message.patch";
+    url = "https://github.com/direnv/direnv/pull/1426.patch";
+    hash = "sha256-qK4wT4+jZdzLBWm5m0up/VZWiUw9kzS9FhM0NsC/DZo=";
+  })];
+})
