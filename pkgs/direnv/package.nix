63ea1bd4385f09e00f486c65a01b6c26988c7f4b
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-06-13 11:37:19 -0700

    nix fmt

--- pkgs/direnv/package.nix
+++ pkgs/direnv/package.nix
@@ -4,9 +4,11 @@
 }:
 
 direnv.overrideAttrs (oldAttrs: {
-  patches = (oldAttrs.patches or []) ++ [(fetchpatch {
-    name = "error-message.patch";
-    url = "https://github.com/direnv/direnv/pull/1426.patch";
-    hash = "sha256-qK4wT4+jZdzLBWm5m0up/VZWiUw9kzS9FhM0NsC/DZo=";
-  })];
+  patches = (oldAttrs.patches or [ ]) ++ [
+    (fetchpatch {
+      name = "error-message.patch";
+      url = "https://github.com/direnv/direnv/pull/1426.patch";
+      hash = "sha256-qK4wT4+jZdzLBWm5m0up/VZWiUw9kzS9FhM0NsC/DZo=";
+    })
+  ];
 })
