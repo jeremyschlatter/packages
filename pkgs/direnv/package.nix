14b0afa7195a74e30338ea403f80d20c5992ce66
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-07-24 10:41:33 -0700

    direnv: patches have been released upstream

--- pkgs/direnv/package.nix
+++ pkgs/direnv/package.nix
@@ -2,9 +2,4 @@
   direnv,
 }:
 
-direnv.overrideAttrs (oldAttrs: {
-  src = oldAttrs.src.override {
-    rev = "63e95577b0f4c6fc3d9b23753c7d1c9e579c1773";
-    hash = "sha256-7gJ6B742umSJN1taeRs1C0okilwYfeMysMRn3nIy7hA=";
-  };
-})
+direnv
